6ab4836fa2967760bf302e8a179afbf99fd98435
quangthu2007dt <quangthu2007dt@users.noreply.github.com>
2026-03-02 00:38:14 +0700

    revert: step 2.1 ds.txt scaffold

--- DANG NHAP FACEBOOK/Form1.cs
+++ DANG NHAP FACEBOOK/Form1.cs
@@ -2,24 +2,9 @@ namespace DANG_NHAP_FACEBOOK
 {
     public partial class Form1 : Form
     {
-        private readonly string dsFilePath;
-
         public Form1()
         {
             InitializeComponent();
-
-            dsFilePath = Path.Combine(AppContext.BaseDirectory, "ds.txt");
-            DamBaoTonTaiDsTxt();
-        }
-
-        private void DamBaoTonTaiDsTxt()
-        {
-            if (File.Exists(dsFilePath))
-            {
-                return;
-            }
-
-            File.WriteAllText(dsFilePath, string.Empty);
         }
     }
 }
